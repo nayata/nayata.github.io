8ff216ed9ae811e22072f932565c7581cf832e91
Vladimir Nayata <catapon.games@gmail.com>
2025-04-25 23:48:52 +0300

    Update format.js

--- format/format.js
+++ format/format.js
@@ -1,7 +1,8 @@
 window.storyFormat({
 	name: 'My Story Format',
-	version: '1.0.0',
-	editorExtensions: {
+	version: '1.2.0',
+	source: '<!DOCTYPE html>\n<html>\n\t<head>\n\t\t<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n\t\t<meta charset=\"utf-8\"/>\n\t\t<title>{{STORY_NAME}}</title>\n\t</head>\n\t<body>\n\t\t{{STORY_DATA}}\n\t\t<page>\n\t\t</page>\n\t</body>\n</html>',
+ 	editorExtensions: {
 		twine: {
 			'^2.4.0-alpha1': {
 				codeMirror: {
