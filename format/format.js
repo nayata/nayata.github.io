75cc5090be594590b6a1aee9641c885ce5d25379
Vladimir Nayata <catapon.games@gmail.com>
2025-04-26 01:54:27 +0300

    Update format.js

--- format/format.js
+++ format/format.js
@@ -1,48 +1,51 @@
 window.storyFormat({
 	name: 'My Story Format',
-	version: '1.3.0',
+	version: '1.4.0',
 	source: '<!DOCTYPE html>\n<html>\n\t<head>\n\t\t<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n\t\t<meta charset=\"utf-8\"/>\n\t\t<title>{{STORY_NAME}}</title>\n\t</head>\n\t<body>\n\t\t{{STORY_DATA}}\n\t\t<page>\n\t\t</page>\n\t</body>\n</html>',
  	editorExtensions: {
 		twine: {
 			'^2.4.0-alpha1': {
 				codeMirror: {
-					commands: {
-						upperCase(editor) {
-							const doc = editor.getDoc();
-
-							doc.replaceSelection(doc.getSelection().toUpperCase());
-						}
-					},
 					mode() {
 						return {
 							startState() {
 								return {};
 							},
 							token(stream, state) {
+								if (stream.match(/\[[^\]]+\]/g)) {
+									return 'variable-2';
+								}
+								if (stream.match(/^-[^:]*(:.*)$/)) {
+									return 'variable-2';
+								}
+								if (/^\/\//.test(stream)) {
+									return 'bracket';
+								}
+
 								stream.skipToEnd();
-								return 'keyword';
+								return 'text';
 							}
 						};
-					},
-					toolbar(editor, environment) {
-						return [
-							{
-								type: 'button',
-								command: 'upperCase',
-								icon: 'data:image/svg+xml,...',
-								label: 'Uppercase Text'
-							}
-						];
 					}
 				},
 				references: {
 					parsePassageText(text) {
 						const results = [];
 
-						const match = text.match(/\[move\s*(.+?)\s*]/);
-						if (match) {
-							results.push(match[1]);
+						const move = text.match(/\[move\s*(.+?)\s*]/);
+						if (move) {
+						  results.push(move[1]);
 						}
+						
+						const route = text.match(/^-.*?:\s*([^\s:].*?)(?=\s*:|$)/);
+						if (route) {
+						  const action = route[1].match(/(=|\+|-|\*|\/| is | roll | chance )/);
+						
+						  if (!action) {
+							results.push(route[1]); 
+						  }
+						}
+
 						return results;
 					}
 				}
