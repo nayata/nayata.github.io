9f86729f167f12eade7ad807340dfdd97502db39
Vladimir Nayata <catapon.games@gmail.com>
2025-04-25 23:45:56 +0300

    Update format.js

--- format/format.js
+++ format/format.js
@@ -1,19 +1,49 @@
 window.storyFormat({
-  "name": "PassageFormat",
-  "version": "1.1.0",
-  "source": "<!DOCTYPE html>\n<html>\n\t<head>\n\t\t<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n\t\t<meta charset=\"utf-8\"/>\n\t\t<title>{{STORY_NAME}}</title>\n\t</head>\n\t<body>\n\t\t{{STORY_DATA}}\n\t\t<page>\n\t\t</page>\n\t</body>\n</html>",
-  "editorExtensions": {
-	"twine": {
-	"^2.4.0-alpha1": {
-			"references": {
-				parsePassageText(text) {
-					const match = text.match(/\[move\s*(.+?)\s*]/);
-					if (match) {
-						return match[1];
+	name: 'My Story Format',
+	version: '1.0.0',
+	editorExtensions: {
+		twine: {
+			'^2.4.0-alpha1': {
+				codeMirror: {
+					commands: {
+						upperCase(editor) {
+							const doc = editor.getDoc();
+
+							doc.replaceSelection(doc.getSelection().toUpperCase());
+						}
+					},
+					mode() {
+						return {
+							startState() {
+								return {};
+							},
+							token(stream, state) {
+								stream.skipToEnd();
+								return 'keyword';
+							}
+						};
+					},
+					toolbar(editor, environment) {
+						return [
+							{
+								type: 'button',
+								command: 'upperCase',
+								icon: 'data:image/svg+xml,...',
+								label: 'Uppercase Text'
+							}
+						];
 					}
-					return text;
+				},
+				references: {
+					parsePassageText(text) {
+						const match = text.match(/\[move\s*(.+?)\s*]/);
+						if (match) {
+							return match[1];
+						}
+						return text;
+					}
 				}
 			}
-	}	
-  }
+		}
+	}
 });
