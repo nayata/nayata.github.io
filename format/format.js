bbc5efb0098d9b07b0d361e69807231f47d684bf
Vladimir Nayata <catapon.games@gmail.com>
2025-04-26 02:33:23 +0300

    Update format.js

--- format/format.js
+++ format/format.js
@@ -1,36 +1,24 @@
 window.storyFormat({
 	name: 'My Story Format',
-	version: '1.5.4',
+	version: '1.6.0',
 	source: '<!DOCTYPE html>\n<html>\n\t<head>\n\t\t<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n\t\t<meta charset=\"utf-8\"/>\n\t\t<title>{{STORY_NAME}}</title>\n\t</head>\n\t<body>\n\t\t{{STORY_DATA}}\n\t\t<page>\n\t\t</page>\n\t</body>\n</html>',
  	editorExtensions: {
 		twine: {
 			'^2.4.0-alpha1': {
 				references: {
 					parsePassageText(text) {
+						const matchers = [/\[move\s*(.+?)\s*]/g, /^-.*?:\s*([^\s:].*?)(?=\s*:|$)/g];
 						const results = [];
 
-						return text.match(/\[move\s*(.+?)\s*]/) || [];
+						for (const matcher of matchers) {
+							let match;
 
-						const moves = text.match(/\[move\s*(.+?)\s*]/);
-						if (moves) {
-							for (const move of moves) {
-						  		results.push(move);
+							while ((match = matcher.exec(text))) {
+								const action = match[1].match(/(=|\+|-|\*|\/| is | roll | chance )/);
+								if (!action) {
+									results.push(match[1]);
+								}
 							}
-						}
-
-						let match;
-
-						//while ((match = text.match(/\[move\s*(.+?)\s*]/))) {
-							//results.push(match[1]);
-						//}
-						
-						const route = text.match(/^-.*?:\s*([^\s:].*?)(?=\s*:|$)/);
-						if (route) {
-						  const action = route[1].match(/(=|\+|-|\*|\/| is | roll | chance )/);
-						
-						  if (!action) {
-							results.push(route[1]); 
-						  }
 						}
 
 						return results;
