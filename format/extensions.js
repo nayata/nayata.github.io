c6ed78edf36760606077fe7cb099b14d48af6149
Vladimir Nayata <catapon.games@gmail.com>
2025-04-25 23:37:07 +0300

    Update extensions.js

--- format/extensions.js
+++ format/extensions.js
@@ -1,6 +1,6 @@
 window.storyFormat({
   "name": "PassageFormat",
-  "version": "1.0.0",
+  "version": "1.1.0",
   "source": "<!DOCTYPE html>\n<html>\n\t<head>\n\t\t<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n\t\t<meta charset=\"utf-8\"/>\n\t\t<title>{{STORY_NAME}}</title>\n\t</head>\n\t<body>\n\t\t{{STORY_DATA}}\n\t\t<page>\n\t\t</page>\n\t</body>\n</html>",
   "editorExtensions": {
 	"twine": {
