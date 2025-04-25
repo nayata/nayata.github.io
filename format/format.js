2e45390e1be94730d39207ea407436c4c55e7135
Vladimir Nayata <catapon.games@gmail.com>
2025-04-26 02:18:12 +0300

    Update format.js

--- format/format.js
+++ format/format.js
@@ -1,6 +1,6 @@
 window.storyFormat({
 	name: 'My Story Format',
-	version: '1.5.2',
+	version: '1.5.3',
 	source: '<!DOCTYPE html>\n<html>\n\t<head>\n\t\t<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n\t\t<meta charset=\"utf-8\"/>\n\t\t<title>{{STORY_NAME}}</title>\n\t</head>\n\t<body>\n\t\t{{STORY_DATA}}\n\t\t<page>\n\t\t</page>\n\t</body>\n</html>',
  	editorExtensions: {
 		twine: {
@@ -9,11 +9,11 @@ window.storyFormat({
 					parsePassageText(text) {
 						const results = [];
 
-						const moves = text.match(/\[move\s*(.+?)\s*]/);
-						if (moves) {
-							for (const move of moves) {
-						  		results.push(move);
-							}
+
+						let match;
+
+						while ((match = text.match(/\[move\s*(.+?)\s*]/))) {
+							results.push(match[1]);
 						}
 						
 						const route = text.match(/^-.*?:\s*([^\s:].*?)(?=\s*:|$)/);
