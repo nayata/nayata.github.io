cd223f81034c3863815e6cd9e1eb75638ddf1734
Vladimir Nayata <catapon.games@gmail.com>
2025-04-26 12:53:09 +0300

    Update format.js

--- format/format.js
+++ format/format.js
@@ -1,10 +1,41 @@
 window.storyFormat({
 	name: 'My Story Format',
-	version: '1.6.6',
+	version: '1.6.7',
 	source: '<!DOCTYPE html>\n<html>\n\t<head>\n\t\t<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n\t\t<meta charset=\"utf-8\"/>\n\t\t<title>{{STORY_NAME}}</title>\n\t</head>\n\t<body>\n\t\t{{STORY_DATA}}\n\t\t<page>\n\t\t</page>\n\t</body>\n</html>',
  	editorExtensions: {
 		twine: {
 			'^2.4.0-alpha1': {
+				codeMirror: {
+					mode() {
+						return {
+							startState() {
+								return {};
+							},
+							token(stream, state) {
+								// Event and Choice
+								if (stream.sol() && stream.match(/^\[[^[].*\]$/)) {
+									return 'keyword';
+								}
+								if (stream.sol() && stream.match(/^-[^:]*(:.*)$/)) {
+									return 'keyword';
+								}
+					
+								// Are we at an insert?
+								if (stream.match(/^\[.+?\]/)) {
+									return 'keyword';
+								}
+					
+								// Try scanning forward to an insert or link.
+								if (stream.eatWhile(/[^[{]/)) {
+									return 'text';
+								}
+
+								stream.skipToEnd();
+								return 'text';
+							}
+						};
+					}
+				},
 				references: {
 					parsePassageText(text) {
 						const matchers = [/\[move\s*(.+?)\s*]/g, /^-[^:\n]*:\s*([^\n:]*)(?=\s*:|\n|$)/gm];
@@ -12,9 +43,11 @@ window.storyFormat({
 
 						for (const matcher of matchers) {
 							let match;
-
 							while ((match = matcher.exec(text))) {
-								results.push(match[1].trim());
+								const action = match[1].match(/(=|\+|-|\*|\/| is | roll | chance )/);
+								if (!action) {
+									results.push(match[1].trim());
+								}
 							}
 						}
 
