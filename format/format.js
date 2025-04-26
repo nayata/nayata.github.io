8c4ccfdaeafb0f6fdac0b077e99539dd5e96559a
Vladimir Nayata <catapon.games@gmail.com>
2025-04-26 21:18:02 +0300

    Update format.js

--- format/format.js
+++ format/format.js
@@ -1,6 +1,6 @@
 window.storyFormat({
 	name: 'My Story Format',
-	version: '1.7.9',
+	version: '1.8.0',
 	source: '<!DOCTYPE html>\n<html>\n\t<head>\n\t\t<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n\t\t<meta charset=\"utf-8\"/>\n\t\t<title>{{STORY_NAME}}</title>\n\t</head>\n\t<body>\n\t\t{{STORY_DATA}}\n\t\t<page>\n\t\t</page>\n\t</body>\n</html>',
  	editorExtensions: {
 		twine: {
@@ -38,7 +38,7 @@ window.storyFormat({
 						return [
 							{
 								type: 'menu',
-								icon: `data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='none' stroke='${iconColor}' strokeWidth='1.5' strokeLinecap='round' strokeLinejoin='round'%3E %3Cpath d='M3 3m0 2a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v14a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2z' /%3E %3Cpath d='M9 13h-2' /%3E %3Cpath d='M13 10h-6' /%3E %3Cpath d='M11 7h-4' /%3E %3C/svg%3E`,
+								icon: `data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='44' height='44' viewBox='0 0 24 24' fill='none' stroke='${iconColor}' strokeWidth='2' strokeLinecap='round' strokeLinejoin='round'%3E %3Cpath d='M3 3m0 2a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v14a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2z' /%3E %3Cpath d='M9 13h-2' /%3E %3Cpath d='M13 10h-6' /%3E %3Cpath d='M11 7h-4' /%3E %3C/svg%3E`,
 								label: 'Commands',
 								items: [
 									{
@@ -77,7 +77,7 @@ window.storyFormat({
 							},
 							{
 								type: 'menu',
-								icon: 'data:image/svg+xml,%3Csvg%20%20xmlns=%22http://www.w3.org/2000/svg%22%20%20width=%2224%22%20%20height=%2224%22%20%20viewBox=%220%200%2024%2024%22%20%20fill=%22none%22%20%20stroke=%22currentColor%22%20%20stroke-width=%222%22%20%20stroke-linecap=%22round%22%20%20stroke-linejoin=%22round%22%20%20class=%22icon%20icon-tabler%20icons-tabler-outline%20icon-tabler-book-2%22%3E%3Cpath%20stroke=%22none%22%20d=%22M0%200h24v24H0z%22%20fill=%22none%22/%3E%3Cpath%20d=%22M19%204v16h-12a2%202%200%200%201%20-2%20-2v-12a2%202%200%200%201%202%20-2h12z%22%20/%3E%3Cpath%20d=%22M19%2016h-12a2%202%200%200%200%20-2%202%22%20/%3E%3Cpath%20d=%22M9%208h6%22%20/%3E%3C/svg%3E',
+								icon: `data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='44' height='44' viewBox='0 0 44 24' fill='none' stroke='${iconColor}' strokeWidth='2' strokeLinecap='round' strokeLinejoin='round'%3E %3Cpath d='M3 3m0 2a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v14a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2z' /%3E %3Cpath d='M9 13h-2' /%3E %3Cpath d='M13 10h-6' /%3E %3Cpath d='M11 7h-4' /%3E %3C/svg%3E`,
 								label: 'Modifiers',
 								items: [
 									{
