948bce974bee780611a529f5011bf2eb2d9e12e0
Vladimir Nayata <catapon.games@gmail.com>
2025-04-27 21:59:03 +0300

    Update format.js

--- format/format.js
+++ format/format.js
@@ -1,6 +1,6 @@
 window.storyFormat({
 	name: 'My Story Format',
-	version: '1.4.7',
+	version: '1.4.8',
 	source: '<!DOCTYPE html>\n<html>\n\t<head>\n\t\t<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n\t\t<meta charset=\"utf-8\"/>\n\t\t<title>{{STORY_NAME}}</title>\n\t</head>\n\t<body>\n\t\t{{STORY_DATA}}\n\t\t<page>\n\t\t</page>\n\t</body>\n</html>',
  	editorExtensions: {
 		twine: {
@@ -11,14 +11,13 @@ window.storyFormat({
 							editor.focus();
 
 							editor.replaceSelection('[move route]', 'start');
-							editor.setCursor(editor.getCursor().line, '[move route]'.length - 1);
+							editor.setCursor('[move route]'.length);
 
 						},
 						insertLoad(editor) {
-							editor.focus();
-
 							editor.replaceSelection('[move route]');
-							editor.setCursor(editor.getCursor().line+1, 0);
+							editor.setCursor(editor.getCursor().line, '[move route]'.length);
+							editor.focus();
 						},
 
 						insertIf(editor) {
