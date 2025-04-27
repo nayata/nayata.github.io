bdafd4b614b4109e83eaeefed6c2e71885f94b31
Vladimir Nayata <catapon.games@gmail.com>
2025-04-27 15:18:48 +0300

    Update format.js

--- format/format.js
+++ format/format.js
@@ -1,6 +1,6 @@
 window.storyFormat({
 	name: 'My Story Format',
-	version: '1.9.6',
+	version: '1.9.7',
 	source: '<!DOCTYPE html>\n<html>\n\t<head>\n\t\t<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n\t\t<meta charset=\"utf-8\"/>\n\t\t<title>{{STORY_NAME}}</title>\n\t</head>\n\t<body>\n\t\t{{STORY_DATA}}\n\t\t<page>\n\t\t</page>\n\t</body>\n</html>',
  	editorExtensions: {
 		twine: {
@@ -128,9 +128,7 @@ window.storyFormat({
 					mode() {
 						return {
 							startState() {
-								return {
-									choice: false
-								};
+								return {};
 							},
 							token(stream, state) {
 								if (stream.sol() && stream.peek() == "-") {
@@ -142,7 +140,7 @@ window.storyFormat({
 
 									stream.skipTo(':');
 									//return 'text';
-									stream.eatWhile(stream.next() != null);
+									//stream.eatWhile(stream.next() != null);
 									return 'keyword';
 									//stream.skipToEnd();
 
