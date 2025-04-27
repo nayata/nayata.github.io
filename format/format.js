47b7bc8bc430ce55c561a13889ac553b69e0ecb1
Vladimir Nayata <catapon.games@gmail.com>
2025-04-27 14:04:39 +0300

    Update format.js

--- format/format.js
+++ format/format.js
@@ -1,6 +1,6 @@
 window.storyFormat({
 	name: 'My Story Format',
-	version: '1.9.6',
+	version: '1.9.7',
 	source: '<!DOCTYPE html>\n<html>\n\t<head>\n\t\t<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n\t\t<meta charset=\"utf-8\"/>\n\t\t<title>{{STORY_NAME}}</title>\n\t</head>\n\t<body>\n\t\t{{STORY_DATA}}\n\t\t<page>\n\t\t</page>\n\t</body>\n</html>',
  	editorExtensions: {
 		twine: {
@@ -140,6 +140,9 @@ window.storyFormat({
 									if (stream.skipTo(':')) {
 										stream.skipToEnd();
 										return 'keyword';
+									}
+									else {
+										return 'text';
 									}
 									//return 'keyword';
 								}
