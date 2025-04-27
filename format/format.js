7b1e15e626ae24fcf837f933ea2f802e13528e74
Vladimir Nayata <catapon.games@gmail.com>
2025-04-27 15:08:41 +0300

    Update format.js

--- format/format.js
+++ format/format.js
@@ -1,6 +1,6 @@
 window.storyFormat({
 	name: 'My Story Format',
-	version: '1.9.4',
+	version: '1.9.5',
 	source: '<!DOCTYPE html>\n<html>\n\t<head>\n\t\t<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n\t\t<meta charset=\"utf-8\"/>\n\t\t<title>{{STORY_NAME}}</title>\n\t</head>\n\t<body>\n\t\t{{STORY_DATA}}\n\t\t<page>\n\t\t</page>\n\t</body>\n</html>',
  	editorExtensions: {
 		twine: {
@@ -134,17 +134,14 @@ window.storyFormat({
 							},
 							token(stream, state) {
 								if (stream.sol() && stream.peek() == "-") {
-									//choice = stream.skipTo(':');
+									choice = stream.skipTo(':');
 									//return 'text';
-									stream.skipTo(':');
+									//stream.skipTo(':');
 
-									if (stream.match(/^[^:]*:\s*(.*)$/)) {
-										stream.skipToEnd();
-										return 'keyword';
-									}
+
 
 									//stream.skipTo(':');
-									return 'text';
+									//return 'text';
 
 									//stream.eatWhile(stream.next() != ":");
 									//stream.skipToEnd();
@@ -155,6 +152,9 @@ window.storyFormat({
 									//}
 										//return 'keyword';
 									
+								}
+								if (choice && stream.match(/^[^:]*:\s*(.*)$/)) {
+									return 'keyword';
 								}
 
 								// Are we at an insert?
