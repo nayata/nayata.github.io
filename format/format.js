2b866543d677085ec6075c8505515e8ba4340a3b
Vladimir Nayata <catapon.games@gmail.com>
2025-04-27 13:24:51 +0300

    Update format.js

--- format/format.js
+++ format/format.js
@@ -1,6 +1,6 @@
 window.storyFormat({
 	name: 'My Story Format',
-	version: '1.9.1',
+	version: '1.9.2',
 	source: '<!DOCTYPE html>\n<html>\n\t<head>\n\t\t<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n\t\t<meta charset=\"utf-8\"/>\n\t\t<title>{{STORY_NAME}}</title>\n\t</head>\n\t<body>\n\t\t{{STORY_DATA}}\n\t\t<page>\n\t\t</page>\n\t</body>\n</html>',
  	editorExtensions: {
 		twine: {
@@ -132,10 +132,8 @@ window.storyFormat({
 							},
 							token(stream, state) {
 								if (stream.sol() && stream.next() == "-") {
-									if (stream.match(":")) {
-										stream.skipTo(':');
-										return 'keyword';
-									}
+									stream.skipTo(':');
+									return 'keyword';
 								}
 
 								// Are we at an insert?
